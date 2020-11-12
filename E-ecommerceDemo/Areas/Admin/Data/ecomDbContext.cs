8c745fa6b538b969d23b124bb5cdbe501f6ada92
maamoun morgan <maamounmorgan@gmail.com>
2020-11-12 18:25:54 +0200

    firstcooit(create project)

---
+++ E-ecommerceDemo/Areas/Admin/Data/ecomDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ecommerceDemo.Areas.Admin.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerceDemo.Models
+{
+    public class ecommerceDbContext : DbContext
+    {
+        public ecommerceDbContext(DbContextOptions<ecommerceDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ E-ecommerceDemo/Areas/Admin/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerceDemo.Areas.Admin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public string Tag { get; set; }
+        public string Desc { get; set; }
+
+
+        //هوا لازم أعمل فيو موديل علشان احط فيها البروبيرتي دي 
+        public IFormFile file {get; set; }
+
+    }
+}
